c379685c295c794e396bc12055f79767c18e6a6e
yusufabusaad <yusufabusaad@users.noreply.github.com>
2021-04-09 00:04:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -77,13 +77,18 @@ function getLocation(event) {
     " | קו אורך : " + lng;
 
     var map = L.map('googleMap').setView([lat, lng], 17);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+ /* L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     maxZoom : 18,
     tileSize:512,
     zoomOffset :-1
 
   }).addTo(map);
+*/
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+                maxZoom: 20,
+                attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+            }).addTo(map);
 L.marker([lat,lng]).addTo(map)
 }
 
