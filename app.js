487d586b4df5df66366a126984ef92bb8364ff25
yusufabusaad <yusufabusaad@users.noreply.github.com>
2021-04-08 22:52:13 +0300

    update

--- app.js
+++ app.js
@@ -73,8 +73,23 @@ function getLocation(event) {
     x.value = "קו רוחב : " + position.coords.latitude +
     " | קו אורך : " + position.coords.longitude;
 
+    // Initialize and add the map
+  // The location of Uluru
+  const uluru = { lat: position.coords.latitude, lng: position.coords.longitude };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("googleMap"), {
+    zoom: 18,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
     
-  }
+
 
 
   
