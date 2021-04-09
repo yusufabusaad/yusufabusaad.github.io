717ef7e4545a927c1ed3bc3c56877fa97825026f
yusufabusaad <yusufabusaad@users.noreply.github.com>
2021-04-09 16:18:25 +0300

    v2

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-function showPicture() 
+var firebaseConfig = {
+  apiKey: "AIzaSyBG1wOKzTdOSqtRw_5beSfq_OpaBMWmEU0",
+  authDomain: "myapp-a869f.firebaseapp.com",
+  projectId: "myapp-a869f",
+  storageBucket: "myapp-a869f.appspot.com",
+  messagingSenderId: "290342628514",
+  appId: "1:290342628514:web:32af31be2f09b3be7dfbfb"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var ID = firebase.database().ref().push().key;
+
+
+
+function showPicture(event) 
 {
     let preview = document.querySelector('#preview');
 
@@ -11,14 +25,17 @@ function showPicture()
         if (!/\.(jpe?g|png|gif)$/i.test(file.name)) {
             return alert(file.name + " is not an image");
           } 
-
+         var f = event.target.files;
+         console.log(f);
           var reader = new FileReader();
           reader.addEventListener('load',function() {
               let image = new Image();
-              image.className = 'mx-1  image is-96x96'
+              image.className = 'image  is-128x128'
               image.style.borderRadius = "5px";
               image.style.border = "1px dashed #48c774";
-              image.style.padding = "2px";
+              image.style.padding = "1px";
+              image.style.marginLeft = "2px";
+              image.style.marginTop = "2px";
               image.title = file.name;
               image.src = this.result;
               //console.log(image.outerHTML)
@@ -31,10 +48,17 @@ function showPicture()
               }
            
               preview.appendChild(image);
+              var imageURL = []
+              function uploadPhoto()
+                  {
+                   var uploadTask =  firebase.storage().ref('images/'+ID+"/"+file.name).put(f[0]);
+                  }
+                 document.querySelector("#insert").addEventListener('click',uploadPhoto)
           });
 
           reader.readAsDataURL(file)
     }
+    
 }
 let x = document.querySelector("#MAP");
 function getLocation(event) {
@@ -77,11 +101,14 @@ function getLocation(event) {
     x.value = "קו רוחב : " + lat +
     " | קו אורך : " + lng;
     var map = new ol.Map({
+      controls: [],
+      interactions: [],
       target: 'googleMap',
       layers: [
         new ol.layer.Tile({
           source: new ol.source.OSM()
         }),
+        
         new ol.layer.Vector({
           source: new ol.source.Vector({
             features: [
@@ -101,6 +128,59 @@ function getLocation(event) {
 
 }
 
+document.querySelector('#file-input').addEventListener('change',showPicture);
+document.querySelector('#getLocation').addEventListener('click',getLocation);
+
+
+var Lname,Lnumber,Hnumber,MAP,idNumber;
+function ready()
+{
+Lname = document.getElementById("Lname").value; 
+Lnumber = document.getElementById("Lnumber").value;
+Hnumber= document.getElementById("Hnumber").value;
+MAP = document.getElementById("MAP").value;
+idNumber = document.getElementById("idNumber").value;
+}
+
+function validate()
+{
+   if(document.getElementById("Lname").value == '' || document.getElementById("Hnumber").value == '' || document.getElementById("idNumber").value == '' )
+   {
+    return false
+   }
+
+  return true;
+}
+function insertData(event)
+{
+event.preventDefault()
+
+if(!validate())
+{
+ return
+}else {
+  ready();
+}
+
+  firebase.database().ref('data/'+ ID).set({
+    תעודת_זהות: idNumber,
+    ישוב : Lname,
+    שכונה :Lnumber,
+    מספר_בור : Hnumber,
+    מפה : MAP 
+  })
+
+  clearInputs();
+}
+
+function clearInputs() {
+  document.getElementById("Lname").value = '';
+  document.getElementById("Lnumber").value = '';
+  document.getElementById("Hnumber").value = '';
+  document.getElementById("MAP").value = '';
+}
+document.querySelector("#insert").addEventListener('click',insertData)
+document.querySelector("#clearInput").addEventListener('click',clearInputs)
     
 
 
@@ -108,5 +188,3 @@ function getLocation(event) {
   
 
 
-document.querySelector('#file-input').addEventListener('change',showPicture);
-document.querySelector('#getLocation').addEventListener('click',getLocation);
